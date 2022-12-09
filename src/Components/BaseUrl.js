0135896bcf38f8a6ec80ab85956819680e194820
rehanButtCoder <rehan.butt15555@gmail.com>
2022-12-09 15:08:39 +0500

    Changed baseUrl

--- src/Components/BaseUrl.js
+++ src/Components/BaseUrl.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const BaseUrl = axios.create({
-  baseURL: "http://18.218.169.89:81",
+  baseURL: "http://api.miftaahportal.org/",
   headers: {
     "Cache-Control": "no-cache",
     Pragma: "no-cache",
